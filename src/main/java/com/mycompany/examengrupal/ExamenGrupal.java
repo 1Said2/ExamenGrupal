38b4c3719a8b5f9e4eb89dda9725a6a21c278471
Said Cotacachi <oscotacachiu@utn.edu.ec>
2025-05-16 16:51:26 -0500

    Ordenamiento Burbuja implementado

--- src/main/java/com/mycompany/examengrupal/ExamenGrupal.java
+++ src/main/java/com/mycompany/examengrupal/ExamenGrupal.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.examengrupal;
 
 /**
@@ -12,5 +11,19 @@ public class ExamenGrupal {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public static void ordenarPorMetodoBurbuja(int[] arregloNumeros) {
+        int cantidadElementos = arregloNumeros.length;
+        for (int pasada = 0; pasada < cantidadElementos - 1; pasada++) {
+            for (int indiceActual = 0; indiceActual < cantidadElementos - 1; indiceActual++) {
+                int indiceSiguiente = indiceActual + 1;
+                if (arregloNumeros[indiceActual] > arregloNumeros[indiceSiguiente]) {
+                    int valorTemporal = arregloNumeros[indiceActual];
+                    arregloNumeros[indiceActual] = arregloNumeros[indiceSiguiente];
+                    arregloNumeros[indiceSiguiente] = valorTemporal;
+                }
+            }
+        }
     }
 }
